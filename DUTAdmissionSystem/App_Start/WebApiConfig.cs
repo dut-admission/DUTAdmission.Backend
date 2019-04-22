0abd6252ea058d930350ae61d6b7feac086f7cd6
Tram Huynh <huynhthidieutram97@gmail.com>
2019-04-22 20:37:53 +0700

    create get list AdmissionNews API

--- DUTAdmissionSystem/App_Start/WebApiConfig.cs
+++ DUTAdmissionSystem/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace DUTAdmissionSystem
 {
@@ -14,11 +16,21 @@ namespace DUTAdmissionSystem
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            //<--  AdmissionNews controller -->
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                "GetAdmissionNewsApi",
+                "api/addmissionnews",
+                new { controller = "AdmissionNews", action = "GetAdmissionNews" },
+                constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) }
             );
+
+            
         }
     }
 }
---
+++ DUTAdmissionSystem/Models/Service/Abstactions/IAdmissionNewsService.cs
@@ -0,0 +1,16 @@
+﻿using DUTAdmissionSystem.Models.Dto.Input;
+using DUTAdmissionSystem.Models.Dto.Output;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DUTAdmissionSystem.Models.Service.Abstactions
+{
+    public interface IAdmissionNewsService
+    {
+        List<AdmissionNewsResponseDto> GetAdmissionNews(AdmissionNewsConditionSearch conditionSearch);
+
+    }
+}
