8152f6b9b31a9de726ccf4b2b700f66cbd4e90f6
Aiden <trinhminhan1996@gmail.com>
2019-05-05 15:21:12 +0700

    Changed namespace at Authentication Areas.

--- DUTAdmissionSystem/Areas/Authentication/Models/Services/Abstractions/IAuthenticationService.cs
+++ DUTAdmissionSystem/Areas/Authentication/Models/Services/Abstractions/IAuthenticationService.cs
@@ -1,16 +1,10 @@
 ﻿using DUTAdmissionSystem.Areas.Authentication.Models.Dtos.InputDtos;
 using DUTAdmissionSystem.Areas.Authentication.Models.Dtos.OutputDtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DUTAdmissionSystem.Areas.Authentication.Models.Services.Abstactions
+namespace DUTAdmissionSystem.Areas.Authentication.Models.Services.Abstractions
 {
     public interface IAuthenticationService
     {
         LoginResponseDto Login(LoginDto dto);
-
     }
 }
