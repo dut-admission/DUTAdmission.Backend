2141b3b41318764e4ba4c564cdc9ea401c58853d
Aiden <trinhminhan1996@gmail.com>
2019-05-05 12:26:11 +0700

    Revert "Merge pull request #12 from dut-admission/feature/update-password"
    This reverts commit df6b893d70de5fb3db895dbf803e6fd550367f53, reversing
    changes made to 7c97c300216b7827a1c3cec1e5262bb2e0538eb4.

--- DUTAdmissionSystem/Areas/Public/Controllers/ProfileController.cs
+++ DUTAdmissionSystem/Areas/Public/Controllers/ProfileController.cs
@@ -1,6 +1,4 @@
-﻿using DUTAdmissionSystem.Areas.Public.Models.Dtos.InputDtos;
-using DUTAdmissionSystem.Areas.Public.Models.Services.Abstactions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,26 +9,6 @@ namespace DUTAdmissionSystem.Areas.Public.Controllers
 {
     public class ProfileController : ApiController
     {
-        private readonly IProfileService _profileService;
-
-        public ProfileController(IProfileService profileService)
-        {
-            _profileService = profileService;
-        }
-
-        [HttpPut]
-        [ActionName("UpdatePassword")]
-        public IHttpActionResult GetSlides([FromBody]UpdatePassword data)
-        {
-            try
-            {
-                return Ok(_profileService.UpdatePassWord(data));
-            }
-            catch (System.Exception e)
-            {
-                return InternalServerError(e);
-            }
-        }
 
     }
 }
