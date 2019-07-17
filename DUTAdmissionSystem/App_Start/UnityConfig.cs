21e3f63b091e158bfa77297d8b49fd8c88ba1132
Minh Hoang <minhhoang97hk@gmail.com>
2019-07-17 19:29:24 +0700

    delete unity

--- DUTAdmissionSystem/App_Start/UnityConfig.cs
+++ DUTAdmissionSystem/App_Start/UnityConfig.cs
@@ -1,9 +1,5 @@
-﻿using DUTAdmissionSystem.Areas.Admin.Models.Services.Abstractions;
-using DUTAdmissionSystem.Areas.Admin.Models.Services.Implementations;
-using DUTAdmissionSystem.Areas.Authentication.Models.Services.Abstractions;
+﻿using DUTAdmissionSystem.Areas.Authentication.Models.Services.Abstractions;
 using DUTAdmissionSystem.Areas.Authentication.Models.Services.Implementations;
-using DUTAdmissionSystem.Areas.Public.Models.Services.Abstractions;
-using DUTAdmissionSystem.Areas.Public.Models.Services.Implementations;
 using DUTAdmissionSystem.Areas.StudentArea.Models.Services.Abstractions;
 using DUTAdmissionSystem.Areas.StudentArea.Models.Services.Implementations;
 using System.Web.Http;
@@ -22,22 +18,9 @@ namespace DUTAdmissionSystem
             // it is NOT necessary to register your controllers
 
             // e.g. container.RegisterType<ITestService, TestService>();
-            container.RegisterType<IAdmissionNewsService, AdmissionNewsService>();
-            container.RegisterType<ISlideService, SlideService>();
-            container.RegisterType<IUniversityInfoService, UniversityInfoService>();
+            
             container.RegisterType<IAuthenticationService, AuthenticationService>();
-            container.RegisterType<IStudentProfileService, StudentProfileService>();
-            container.RegisterType<IContactMessageService, ContactMessageService>();
-            container.RegisterType<IDocumentService, DocumentService>();
-            container.RegisterType<IEmployeeProfileService, EmployeeProfileService>();
-            container.RegisterType<IAdmissionNewsManagerService, AdmissionNewsManagerService>();
-            container.RegisterType<IContactMessageManagerServiece, ContactMessageManagerServiece>();
-            container.RegisterType<ITuitionManagerService, TuitionManagerService>();
-            container.RegisterType<IStudentManagementService, StudentManagementService>();
-            container.RegisterType<IAccountGroupManagementService, AccountGroupManagementService>();
-            container.RegisterType<ITuitionManagementService, TuitionManagementService>();
-            container.RegisterType<IDocumentManagementService, DocumentManagementService>();
-            container.RegisterType<IAccountManagementService, AccountManagementService>();
+           
 
             // version 2 ( An - Hằng -Hoàng)
 
